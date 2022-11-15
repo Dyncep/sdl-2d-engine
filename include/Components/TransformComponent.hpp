e46c938853ad38fcfa93606e214ccb222c68a6ee
dev@dyncep.com <dev@dyncep.com>
2022-11-15 02:41:04 +0100

    Add velocity / speed to TransformComponent

--- include/Components/TransformComponent.hpp
+++ include/Components/TransformComponent.hpp
@@ -1,5 +1,5 @@
-#ifndef DYNCEP_SDL_ENGINE_COMPONENTS_POSITION_COMPONENT_HPP
-#define DYNCEP_SDL_ENGINE_COMPONENTS_POSITION_COMPONENT_HPP
+#ifndef DYNCEP_SDL_ENGINE_COMPONENTS_TRANSFORM_COMPONENT_HPP
+#define DYNCEP_SDL_ENGINE_COMPONENTS_TRANSFORM_COMPONENT_HPP
 
 #include "ECS/Component.hpp"
 #include "glm/vec2.hpp"
@@ -27,15 +27,21 @@ public:
   glm::vec2 pos{};
 
   /**
+   * @velocity the 2d velocity vector
+   */
+  glm::vec2 velocity{};
+
+  /**
+   * @speed value of the speed to be used with the velocity
+   */
+  int speed = 3;
+
+  /**
    * @brief override of update method
    */
   void update() override {
-    this->pos.x++;
-    this->pos.y++;
-    if (this->pos.x >= 400) {
-      this->pos.x = 0;
-      this->pos.y = 0;
-    }
+    pos.x += velocity.x * speed;
+    pos.y += velocity.y * speed;
   }
 };
 } // namespace Dyncep::Components
