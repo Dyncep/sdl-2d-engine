5d5caae0ee1e111aa7b1433a7bd2a937dcbef3ea
dev@dyncep.com <dev@dyncep.com>
2022-11-15 02:21:24 +0100

    Limit fps

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,13 +1,25 @@
 #include "Application.hpp"
-#include <iostream>
+#include <SDL2/SDL_timer.h>
 
 int main() {
   Dyncep::Application app{{400, 400}, "Hi!"};
 
+  const int frame_delay = 1000 / 60;
+  int frame_time = 0;
+  uint32_t frame_start = 0;
+
   while (app.isRunning()) {
+
+    frame_start = SDL_GetTicks();
+
     app.handleEvents();
     app.update();
     app.render();
+
+    frame_time = SDL_GetTicks() - frame_start;
+    if (frame_delay > frame_time) {
+      SDL_Delay(frame_delay - frame_time);
+    }
   }
 
   return 0;
